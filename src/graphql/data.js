f8043d8bbea5f6ced39f3240d078b6f3d86ccfbe
atumv <atumv.gm@gmail.com>
2021-12-26 22:16:09 +0700

    Style changes

--- src/graphql/data.js
+++ src/graphql/data.js
@@ -1,9 +1,9 @@
 let persons = [
-  { id: 1, name: 'John Irving', age: 29 },
-  { id: 2, name: 'Henry Smith', age: 36 },
-  { id: 3, name: 'George Watson', age: 57 },
-  { id: 4, name: 'Michael Fox', age: 61 },
-  { id: 5, name: 'Bill Collins', age: 44 },
+  { id: 1, name: 'Bill Collins', age: 44 },
+  { id: 2, name: 'John Irving', age: 29 },
+  { id: 3, name: 'Michael Fox', age: 41 },
+  { id: 4, name: 'Henry Smith', age: 32 },
+  { id: 5, name: 'George Watson', age: 57 },
 ];
 
 module.exports = persons;
