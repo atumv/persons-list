aabad762c9d7d4f508d5f292d042358fa57a811d
atumv <atumv.gm@gmail.com>
2021-12-26 22:33:12 +0700

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache()
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
@@ -14,5 +14,5 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
